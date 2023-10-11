f7ba89b8fbd1a2fc9e69ea63602f5c22f26aa3e4
Andrew Vardeman <avardeman@agleader.com>
2023-10-10 21:28:39 -0500

    Added license to SettingsForm.cs
    Signed-off-by: Andrew Vardeman <avardeman@agleader.com>

--- Visualizer/UI/SettingsForm.cs
+++ Visualizer/UI/SettingsForm.cs
@@ -1,4 +1,14 @@
-﻿using System.Windows.Forms.VisualStyles;
+﻿/*******************************************************************************
+ * Copyright (c) 2015 AgGateway and ADAPT Contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Andrew Vardeman - Settings for added UI features
+ *******************************************************************************/
+using System.Windows.Forms.VisualStyles;
 using AgGateway.ADAPT.Visualizer.Properties;
 
 namespace AgGateway.ADAPT.Visualizer.UI
